de994fb017c3eaf2511eaf6b7a05fdb0c124bc4c
muriz-dev <mhmdrizki.dev@gmail.com>
2023-11-29 11:18:56 +0700

    build: install preline ui component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './public/**/*.{html,js}'],
+  content: [
+    "./index.html",
+    "./public/**/*.{html,js}",
+    "node_modules/preline/dist/*.js",
+  ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin')
+  ],
+};
